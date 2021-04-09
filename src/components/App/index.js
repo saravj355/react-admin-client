51726bb06da4cf8e4e063566a1769a7402d66f4a
saravj355 <svj971@gmail.com>
2021-04-08 20:59:13 -0500

    Add main page

---
+++ src/components/App/index.js
@@ -0,0 +1,9 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+import * as React from 'react';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+);
