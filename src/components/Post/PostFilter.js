86368663f6171e118158271e23fcc59111588290
saravj355 <svj971@gmail.com>
2021-04-08 20:48:25 -0500

    add filter

---
+++ src/components/Post/PostFilter.js
@@ -0,0 +1,17 @@
+import { Filter, ReferenceInput, SelectInput, TextInput } from 'react-admin';
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput
+            label="User"
+            source="userId"
+            reference="users"
+            allowEmpty
+        >
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export default PostFilter;
